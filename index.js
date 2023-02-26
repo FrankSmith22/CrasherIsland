0e4cd334882ed70e39ee4a441da370bd7ce6b6da
Frank <fasvi6@gmail.com>
2023-02-26 18:29:31 -0500

    Render character

--- index.js
+++ index.js
@@ -9,8 +9,19 @@ c.fillRect(0, 0, canvas.width, canvas.height)
 
 const image = new Image()
 image.src = "./img/crasherIsland.png"
-
+const playerImage = new Image()
+playerImage.src = './img/playerDown.png'
 image.onload = () => {
-    c.drawImage(image, -850, -250)
-
+    c.drawImage(image, -832, -220)
+    c.drawImage(playerImage,
+        0,
+        0,
+        (playerImage.width / 4),
+        (playerImage.height),
+        (canvas.width / 2) - (playerImage.width / 4) / 2,
+        (canvas.height / 2) - (playerImage.height / 2),
+        (playerImage.width / 4),
+        (playerImage.height)
+    )
 }
+
