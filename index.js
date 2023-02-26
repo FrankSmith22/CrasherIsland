94c23823b959d9bf1ab2178fed9b8e2cd0088c91
Frank <fasvi6@gmail.com>
2023-02-26 17:31:00 -0500

    import and render map

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 const canvas = document.querySelector('canvas')
 const c = canvas.getContext('2d')
 
-canvas.width = '1024px'
-canvas.height = '576px'
+canvas.width = '1024'
+canvas.height = '576'
 
+c.fillStyle = 'white'
+c.fillRect(0, 0, canvas.width, canvas.height)
+
+const image = new Image()
+image.src = "./img/crasherIsland.png"
+
+image.onload = () => {
+    c.drawImage(image, -850, -250)
+
+}
