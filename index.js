df8152d8c8b8692638d685e153628e1c46a30a92
Frank <fasvi6@gmail.com>
2023-02-26 19:04:46 -0500

    implement player movement

--- index.js
+++ index.js
@@ -7,12 +7,47 @@ canvas.height = '576'
 c.fillStyle = 'white'
 c.fillRect(0, 0, canvas.width, canvas.height)
 
-const image = new Image()
-image.src = "./img/crasherIsland.png"
+const backgroundImage = new Image()
+backgroundImage.src = "./img/crasherIsland.png"
 const playerImage = new Image()
 playerImage.src = './img/playerDown.png'
-image.onload = () => {
-    c.drawImage(image, -832, -220)
+
+class Sprite {
+    constructor({position, image}) {
+        this.position = position
+        this.image = image
+    }
+
+    draw() {
+        c.drawImage(this.image, this.position.x, this.position.y)
+    }
+}
+const background = new Sprite({
+    position: {
+        x: -832,
+        y: -220
+    },
+    image: backgroundImage
+})
+
+const keys = {
+    w: {
+        pressed: false
+    },
+    a: {
+        pressed: false
+    },
+    s: {
+        pressed: false
+    },
+    d: {
+        pressed: false
+    },
+}
+
+function animate() {
+    window.requestAnimationFrame(animate)
+    background.draw()
     c.drawImage(playerImage,
         0,
         0,
@@ -23,5 +58,42 @@ image.onload = () => {
         (playerImage.width / 4),
         (playerImage.height)
     )
+    if(keys.w.pressed && lastKey == 'w') background.position.y += 3
+    else if(keys.a.pressed && lastKey == 'a') background.position.x += 3
+    else if(keys.s.pressed && lastKey == 's') background.position.y -= 3
+    else if(keys.d.pressed && lastKey == 'd') background.position.x -= 3
 }
+animate()
 
+let lastKey = ''
+window.addEventListener('keydown', (e) => {
+    switch(e.key) {
+        case "w": keys.w.pressed = true
+                  lastKey = 'w'
+                  break;
+        case "a": keys.a.pressed = true
+                  lastKey = 'a'
+                  break;
+        case "s": keys.s.pressed = true
+                  lastKey = 's'
+                  break;
+        case "d": keys.d.pressed = true
+                  lastKey = 'd'
+                  break;
+    }
+    console.log(keys)
+})
+
+window.addEventListener('keyup', (e) => {
+    switch(e.key) {
+        case "w": keys.w.pressed = false
+                  break;
+        case "a": keys.a.pressed = false
+                  break;
+        case "s": keys.s.pressed = false
+                  break;
+        case "d": keys.d.pressed = false
+                  break;
+    }
+    console.log(keys)
+})
