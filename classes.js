eedcb5273b804907caa6212d216bbc5ed76a8a08
Frank <fasvi6@gmail.com>
2023-03-12 16:21:26 -0400

    player movement animation

--- classes.js
+++ classes.js
@@ -1,17 +1,20 @@
 class Sprite {
-    constructor({ position, image, frames = { max: 1 } }) {
+    constructor({ position, image, frames = { max: 1 }, sprites }) {
         this.position = position
         this.image = image
-        this.frames = frames
+        this.frames = { ...frames, val: 0, elapsed: 0 }
         this.image.onload = () => {
             this.width = this.image.width / this.frames.max
             this.height = this.image.height
         }
+        this.moving = false
+        this.sprites = sprites
     }
 
     draw() {
-        c.drawImage(this.image,
-            0,
+        c.drawImage(
+            this.image,
+            this.frames.val * this.width,
             0,
             (this.image.width / this.frames.max),
             (this.image.height),
@@ -20,6 +23,20 @@ class Sprite {
             (this.image.width / this.frames.max),
             (this.image.height)
         )
+
+        if (!this.moving) return
+        if (this.frames.max > 1) {
+            this.frames.elapsed++
+        }
+
+        if (this.frames.elapsed % 10 === 0) {
+            if (this.frames.val < this.frames.max - 1) {
+                this.frames.val++
+            }
+            else {
+                this.frames.val = 0
+            }
+        }
     }
 }
 
@@ -33,7 +50,8 @@ class Boundary {
     }
 
     draw() {
-        c.fillStyle = 'rgba(0,0,0,0)'
+        c.fillStyle = 'rgba(0,0,0,0.3)'
+
         c.fillRect(
             this.position.x,
             this.position.y,
